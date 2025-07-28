f66d90e352f556ab8fa866d9c774525e183165a1
Jarry1222 <William-Champion@outlook.com>
2025-07-27 21:47:31 -0300

    w

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+        Hola Jarry
       </Text>
     </View>
   )
