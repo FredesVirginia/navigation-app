29bc12658c8820c49b6c43fe3dc07376afee4dd8
Jarry1222 <William-Champion@outlook.com>
2025-07-28 23:28:44 -0300

    w

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,8 @@
-import { Redirect } from 'expo-router';
+import CustomButton from '@/components/shared/CustomButton';
+import { Link, router } from 'expo-router';
 import React from 'react';
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 
@@ -7,7 +10,23 @@ const App = () => {
 
 
   return (
-  <Redirect  href="/home"/>
+  <SafeAreaView>
+      <View className='px-10 mt-5'>
+
+        <Link href="/products" asChild>
+         <CustomButton color="primary"  
+      
+        >Productos</CustomButton>
+        </Link>
+        <CustomButton color="primary"  
+        onPress={()=> router.push("/products")}
+        >Productos</CustomButton>
+
+        <CustomButton variant='text-only' color="primary"  
+        onPress={()=> router.push("/products")}
+        >Productos</CustomButton>
+      </View>
+  </SafeAreaView>
   )
 }
 
--- components/shared/CustomButton.tsx
+++ components/shared/CustomButton.tsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { Pressable, PressableProps, Text, View } from "react-native";
 
-const CustomButton = () => {
-  return (
-    <View>
-      <Text>CustomButton</Text>
-    </View>
-  )
+interface Props extends PressableProps {
+  children: string;
+  color: "primary" | "secondary" | "tertiary";
+  variant?: "contained" | "text-only";
 }
+const CustomButton = React.forwardRef(
+  ({ children, color = "primary", onPress, onLongPress, variant = "contained" }: Props, ref: React.Ref<View>) => {
+    const btnColor = {
+      primary: "bg-primary",
+      secondary: "bg-secondary",
+      tertiary: "bg-tertiary",
+    }[color];
 
-export default CustomButton
+     const textColor = {
+      primary: "text-primary",
+      secondary: "text-secondary",
+      tertiary: "text-tertiary",
+    }[color];
+    if (variant === "text-only") {
+      return (
+        <Pressable
+          className={`p-3 `}
+          onPress={onPress}
+          onLongPress={onLongPress}
+          ref={ref}
+        >
+          <Text className= { `text-center  ${textColor} font-work-medium`}>{children}</Text>
+        </Pressable>
+      );
+    }
+
+    return (
+
+      <Pressable className={`p-3 rounded-md ${btnColor}  active:opacity-90 `} onPress={onPress} onLongPress={onLongPress} ref={ref}>
+      <Text className="text-white text-center font-work-medium">{children}</Text>
+    </Pressable>
+    )
+  }
+);
+
+export default CustomButton;
